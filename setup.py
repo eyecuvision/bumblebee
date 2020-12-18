848624df9331fd87d312bbd4ecc8da48a82646cd
Oguz Vuruskaner <ovuruska@outlook.com>
2020-12-18 23:37:38 +0300

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 import setuptools
 
-import setuptools
-
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 __author__ = "Oguz Vuruskaner"
 
 setuptools.setup(
-    name="not-a-bumblebee", # Replace with your own username
+    name="not-a-bumblebee", 
     version="0.0.1",
     author="Oguz Vuruskaner",
     author_email="ovuruska@outlook.com",
@@ -16,7 +14,11 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Eye-C-U/bumblebee",
-    py_modules=["eyecu"],
+    package_data={
+        "eyecu":[
+            "./src/__init__.py"
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

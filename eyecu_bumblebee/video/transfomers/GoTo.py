14c896be04261c0b8a7f1b4b6ebb06c2b3fe2963
Oguz Vuruskaner <ovuruska@outlook.com>
2020-12-25 18:14:50 +0300

    Going to specific frame added.

---
+++ eyecu_bumblebee/video/transfomers/GoTo.py
@@ -0,0 +1,18 @@
+import cv2
+
+from ..streams.FileStream import FileStream
+from ..interfaces.ITransformer import ITransformer
+
+
+class Loop(ITransformer):
+
+    def __init__(self, src: FileStream):
+        super().__init__(src)
+        self.src = src
+
+    def read(self,frame_number=None):
+
+        if frame_number is not None:
+            self.src.cap.set(cv2.CAP_PROP_POS_FRAMES,frame_number)
+
+        return super().read()

214843fe6f07a1da6970ea0446ff96c91560ced8
Oguz Vuruskaner <ovuruska@outlook.com>
2020-12-26 18:04:18 +0300

    Added wrapper to fetch duration of file stream.

--- eyecu_bumblebee/video/streams/FileStream.py
+++ eyecu_bumblebee/video/streams/FileStream.py
@@ -11,7 +11,15 @@ class FileStream(IDataSource):
         self.id = IDataSource.__ID__
         IDataSource.__ID__ +=1
         self.cap = cv2.VideoCapture(self.path)
+        self.cap.open()
 
 
     def __del__(self):
         self.cap.release()
+
+
+    def __len__(self):
+        return self.get_duration()
+
+    def get_duration(self):
+        return self.cap.get(cv2.CAP_PROP_FRAME_COUNT) + 1

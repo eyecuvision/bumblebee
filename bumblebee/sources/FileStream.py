10400bed9ccb9f2fad50d93134bf91ce1a987c51
Oguz Vuruskaner <ovuruska@outlook.com>
2020-12-27 17:21:31 +0300

    Fixed missing required argument error.

--- bumblebee/sources/FileStream.py
+++ bumblebee/sources/FileStream.py
@@ -11,7 +11,7 @@ class FileStream(ISource):
         self.id = ISource.__ID__
         ISource.__ID__ +=1
         self.cap = cv2.VideoCapture(self.path)
-        self.cap.open()
+        self.cap.open(0)
 
 
     def __del__(self):
